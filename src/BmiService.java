86c5cafe6e1db475b336f6db6876ac25e80a7b58
gkz <giorgadzekonstantin@gmail.com>
2023-06-22 14:31:54 +0400

    Changed the names of variables

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double a, int b) {
-        int calculate = (int) (b / (a * a));
+    public int calculate(double height, int weight) {
+        int calculate = (int) (weight / (height * height));
         return calculate;
     }
 }
